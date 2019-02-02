5ee58e7173af78cabb7be4e493c6ce2d89e43633
Brad Searle <bradleysearle@gmail.com>
2019-02-02 13:37:29 +1000

    dev branch

--- lab_base/cli.py
+++ lab_base/cli.py
@@ -5,25 +5,27 @@ if not sys.version_info >= (3, 6):
 import argparse
 import json
 
-from lab_base.vagrant import (
-    get_guests,
-    ssh_config_to_list,
-    worker,
-)
+from lab_base import vagrant
+from lab_base import provision
 
 
 def main():
     parser = argparse.ArgumentParser(description='Lab Base Provisioning')
     parser.add_argument('--ssh-config', default=False, action='store_true',
                         dest='ssh_config', help='Gather Vagrant SSH config')
+    parser.add_argument('--provision', nargs='+', help='Apply config to devices')
     args = parser.parse_args()
 
     if args.ssh_config:
         print('Gathering vagrant SSH config')
-        guests = get_guests()
-        ssh_config_dict = worker(guests)
+        guests = vagrant.get_guests()
+        ssh_config_dict = vagrant.worker(guests)
         with open('.sshconfig', 'w') as f:
-            f.write('\n'.join(ssh_config_to_list(ssh_config_dict)))
+            f.write('\n'.join(vagrant.ssh_config_to_list(ssh_config_dict)))
         with open('.sshconfig.json', 'w') as f:
             f.write(json.dumps(ssh_config_dict))
         print(f'SSH config saved to file ".sshconfig and .sshconfig.json"')
+
+    if args.provision:
+        print('Gathering vagrant SSH config')
+        provision.worker(args.provision)
