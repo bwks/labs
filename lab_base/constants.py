0c58a8f31ec943bb3e686b42eb9b63b2c5d9cd01
Brad Searle <bradleysearle@gmail.com>
2019-10-03 19:18:37 +1000

    utility functions

--- lab_base/constants.py
+++ lab_base/constants.py
@@ -1,6 +1,8 @@
 import os
 
+from nornir import InitNornir
 
+USER_HOME = os.path.expanduser('~')
 BASE_DIR = os.path.join(os.path.dirname(__file__))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATE_MAP = {
@@ -8,4 +10,33 @@ TEMPLATE_MAP = {
     'vqfx': 'juniper-vqfx.j2',
     'veos': 'arista-veos.j2',
     'cvx': 'cumulus-vx.j2',
+}
+
+
+nr = InitNornir(
+    core={"num_workers": 100},
+    inventory={
+        "plugin": "nornir.plugins.inventory.simple.SimpleInventory",
+        "options": {
+            "host_file": "inventory/hosts.yaml",
+            "group_file": "inventory/groups.yaml"
+        }
+    }
+)
+
+groups = {
+    'base': {
+        'connection_options': {
+            'naplam': {
+                'extras': {
+                    'optional_args': {
+                        'ssh_config_file': '.sshconfig',
+                    }
+                }
+            }
+        }
+    },
+    'ospf': {},
+    'isis': {},
+    'mpls': {},
 }
--- lab_base/utils.py
+++ lab_base/utils.py
@@ -1,6 +1,74 @@
 import json
+import yaml
 
 
-def load_ssh_config():
-    with open('.sshconfig.json', 'r') as f:
-        return json.load(f)
+def load_json_file(json_file):
+    """
+    Load a JSON file.
+    :param json_file: name of file
+    :return: dict of data
+    """
+    try:
+        with open(json_file, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
+
+def load_yaml_file(yaml_file='guest.yml'):
+    """
+    Load a YAML file.
+    :param yaml_file: name of file
+    :return: dict of data
+    """
+    try:
+        with open(yaml_file, 'r') as f:
+            return yaml.safe_load(f)
+    except FileNotFoundError:
+        return {}
+
+
+def driver_switcher(guest_model):
+    """
+    Convert Grifter guest type to Napalm driver type.
+    :param guest_model: Grifter guest model
+    :return: Napalm driver string
+    """
+    driver_map = {
+        'arista/veos': 'eos',
+        'cisco/iosv': 'ios',
+        'cisco/iosv-l2': 'ios',
+        'cisco/csr1000v': 'ios',
+        'juniper/vmx-vcp': 'junos',
+        'juniper/vmx-vfp': 'junos',
+        'juniper/vqfx-pfe': 'junos',
+        'juniper/vqfx-re': 'junos',
+        'juniper/vsrx': 'junos',
+        'juniper/vsrx-packetmode': 'junos',
+    }
+    try:
+        return driver_map[guest_model]
+    except KeyError:
+        raise KeyError(f'Guest model: "{guest_model}" does not have an associated driver.')
+
+
+def generate_nornir_inventory():
+    """
+    Generate a Nornir inventory dict
+    :return: Nornir inventory dict
+    """
+    ssh_config = load_json_file('.sshconfig.json')
+    guests = load_yaml_file('guests.yml')
+    inventory = {}
+    for host, data in ssh_config.items():
+        platform = driver_switcher(guests[host]['vagrant_box']['name'])
+        inventory.update({
+            host: {
+                'hostname': host,
+                'username': data['User'],
+                'password': '',
+                'platform': platform,
+                'groups': [f'pod{host[1]}', 'base'],
+            }
+        })
+    return inventory
