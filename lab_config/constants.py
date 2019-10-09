fbe420cfdfd944e8ff2a602aa6ad3cf0506e93d8
Brad Searle <bradleysearle@gmail.com>
2019-10-09 10:45:33 +1000

    started to include labs

--- lab_config/constants.py
+++ lab_config/constants.py
@@ -4,7 +4,7 @@ USER_HOME = os.path.expanduser('~')
 BASE_DIR = os.path.join(os.path.dirname(__file__))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATE_MAP = {
-    'vmx': 'juniper-vmx.j2',
+    'vmx': 'p1r2-vmx.j2',
     'vqfx': 'juniper-vqfx.j2',
     'veos': 'arista-veos.j2',
     'cvx': 'cumulus-vx.j2',
