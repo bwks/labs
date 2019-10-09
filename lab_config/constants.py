c1440528f8ea04b4e5062b5fed7348a0665c87df
Brad Searle <bradleysearle@gmail.com>
2019-10-09 14:15:07 +1000

    ospf lab config

--- lab_config/constants.py
+++ lab_config/constants.py
@@ -4,7 +4,7 @@ USER_HOME = os.path.expanduser('~')
 BASE_DIR = os.path.join(os.path.dirname(__file__))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATE_MAP = {
-    'vmx': 'p1r2-vmx.cfg',
+    'vmx': 'juniper-vmx.cfg',
     'vqfx': 'juniper-vqfx.j2',
     'veos': 'arista-veos.j2',
     'cvx': 'cumulus-vx.j2',
